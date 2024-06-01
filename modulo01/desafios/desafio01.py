4ae899b5f04a98f41b0a070c8dadb1a1f8c140a0
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-05-31 21:07:31 -0300

    upload desafios

---
+++ modulo01/desafios/desafio01.py
@@ -0,0 +1,4 @@
+# crie um código que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = str(input('Qual é o seu nome: '))
+print(f'Olá {nome}! Prazer em te conhecer!')
---
+++ modulo01/desafios/desafio02.py
@@ -0,0 +1,7 @@
+# crie um código que leia o dia, o mes e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+
+dia = int(input('Dia =')) 
+mes = input('Mes =')
+ano = int(input('Ano ='))
+
+print(f'Voce nasceu no dia {dia} de {mes} de {ano}. Correto?')
