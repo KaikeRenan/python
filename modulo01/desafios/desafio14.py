d93f278eac40b55f333234eab1b116597ea39f90
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-06-01 18:57:13 -0300

    upload desafios

---
+++ modulo01/desafios/desafio14.py
@@ -0,0 +1,7 @@
+# crie um programa que converta uma temperatura digita em °C e converta para °F
+
+celsius = float(input('Informe a temperatura em °C: '))
+
+fahrenheit = (celsius*9/5)+32
+
+print(f'A temperatura de {celsius}°C corresponde a {fahrenheit}°F!')
---
+++ modulo01/desafios/desafio15.py
@@ -0,0 +1,12 @@
+# crie um código que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por km rodado
+
+alugado = int(input('Quantos dias alugados? '))
+km = float(input('Quantos Km rodados? '))
+
+custo_por_dia = alugado*60
+
+custo_por_km = km*0.15
+
+total = custo_por_dia + custo_por_km
+
+print(f'O total a pagar é de R${total:.2f}')
