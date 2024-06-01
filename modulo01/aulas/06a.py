5074b8ef3972e2b37fbbc988aeb3e990d6c7ce15
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-05-31 21:07:43 -0300

    aula 6

---
+++ modulo01/aulas/06a.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um valor: '))
+n2 = int(input('digite outro: '))
+
+#print('A soma de {0} e {1} vale {2}'.format(n1, n2, n1+n2))
+
+print(f'A soma de {n1} e {n2} vale {n1+n2}')
---
+++ modulo01/aulas/06b.py
@@ -0,0 +1,4 @@
+n = input('Digite um algo: ')
+print(n.isupper())
+
+#is testa se a variável é ou nao e retorna true ou false
