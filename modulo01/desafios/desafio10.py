48f0ed2b05eced389efde6377c06562cc625a39b
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-06-01 12:55:24 -0300

    Update desafio10.py

--- modulo01/desafios/desafio10.py
+++ modulo01/desafios/desafio10.py
@@ -1,6 +1,6 @@
 # crie um código que leia quanto de dinheiro uma pessoa tem na carteira e mostre quantos doláres ela pode comprar (US$1,00 = R$ 3,27)
 
-carteira = float(input('Digite o quanto de dinheiro voce tem na carteira: '))
+carteira = float(input('Digite o quanto de dinheiro voce tem na carteira: R$'))
 
 dolares = carteira/3.27
 
