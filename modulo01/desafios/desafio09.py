421910b5675d2ff80b6273b071b62945644dfd95
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-06-01 12:55:20 -0300

    Update desafio09.py

--- modulo01/desafios/desafio09.py
+++ modulo01/desafios/desafio09.py
@@ -2,4 +2,4 @@
 
 num = int(input('Digite um número inteiro: '))
 
-print(f'Número escolhido foi {num}\nE sua tabuada é:\n{num}x1 = {num*1}\n{num}x2 = {num*2}\n{num}x3 = {num*3}\n{num}x4 = {num*4}\n{num}x5 = {num*5}\n{num}x6 = {num*6}\n{num}x7 = {num*7}\n{num}x8 = {num*8}\n{num}x9 = {num*9}\n{num}x10 = {num*10}')
+print(f'Número escolhido foi {num}\nE sua tabuada é:\n{num} x  1 = {num*1}\n{num} x  2 = {num*2}\n{num} x  3 = {num*3}\n{num} x  4 = {num*4}\n{num} x  5 = {num*5}\n{num} x  6 = {num*6}\n{num} x  7 = {num*7}\n{num} x  8 = {num*8}\n{num} x  9 = {num*9}\n{num} x 10 = {num*10}')
