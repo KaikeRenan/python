be00ce16d7572ff8d2def02de1e458d200b5e4e9
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-06-01 22:28:43 -0300

    aula 8

---
+++ modulo01/aulas/08a.py
@@ -0,0 +1,22 @@
+# import math
+
+# # from math import sqrt, ceil
+
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+
+# print(f'A raiz {num} é igual a {math.ceil(raiz)}')
+
+#####################################################
+
+# import random
+
+# # num = random.random()
+# num = random.randint(1,10)
+
+# print(num)
+
+#####################################################
+
+import emoji
+print(emoji.emojize('Olá, Mundo! 🌎', language='pt'))
