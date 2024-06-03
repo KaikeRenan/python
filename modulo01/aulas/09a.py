53326c724f3783eef9b5a7e23053be505a287280
Kaike Renan de Almeida <kaikerenan11@gmail.com>
2024-06-03 13:54:19 -0300

    aula 9

---
+++ modulo01/aulas/09a.py
@@ -0,0 +1,69 @@
+frase = 'Curso em Video Python'
+
+### fatiamento de string
+
+# print(frase[9])
+
+# print(frase[9:13])
+# print(frase[9:21])
+
+# print(frase[9:21:2])
+
+# print(frase[:5])
+
+# print(frase[15:])
+
+# print(frase[9::3])
+
+### Análise
+
+# print(len(frase))
+
+# print(frase.count('o'))
+# print(frase.count('o',0,13))
+
+# print(frase.find('deo'))
+# print(frase.find('android'))
+
+# print('Curso' in frase)
+
+### Transformações
+ 
+# print(frase.replace('Python','Android'))
+# print(frase.upper())
+# print(frase.lower())
+# print(frase.capitalize())
+# print(frase.title())
+
+# frase2 ='   Aprenda Pytho  '
+# print(frase2.strip())
+# print(frase2.rstrip())
+# print(frase2.ltrip() )
+
+# divisao 
+
+# print(frase.split())
+
+# junção
+
+# print('-'.join(frase))
+
+# print("""texto""")
+
+# print(frase.upper.count('o'))
+
+# str sao imutaveis
+# ex: 
+# frase = 'Curso em Video Python'
+# frase[0] = 'J'
+# isso da erro
+
+# frase = 'Curso em Video Python'
+# print(frase.find('video'))
+# print(frase.lower().find('video'))
+
+# dividido = frase.split()
+# print(dividido[0])
+
+# dividido = frase.split()
+# print(dividido[2][3])
